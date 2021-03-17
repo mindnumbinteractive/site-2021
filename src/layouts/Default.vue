fd1cff72b5f7833a994524130bdf1dc97393017b
Michael DeLally <thoughtreactor@gmail.com>
2021-03-17 12:49:21 -0400

    initial commit; placeholder site

---
+++ src/layouts/Default.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="layout h-full">
+    <slot />
+  </div>
+</template>
+
+<static-query>
+query {
+  metadata {
+    siteName
+  }
+}
+</static-query>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@200;400;700&display=swap");
+
+body,
+html {
+  height: 100%;
+}
+
+body {
+  background-color: black;
+  color: white;
+
+  font-family: "Nunito Sans", -apple-system, BlinkMacSystemFont, "Segoe UI",
+    Roboto, Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+}
+</style>
---
+++ src/pages/Index.vue
@@ -0,0 +1,19 @@
+<template>
+  <Layout>
+    <div class="flex flex-col justify-center items-center h-full">
+      <g-image alt="Mindnumb" src="~/assets/logo.png" width="420" />
+
+      <h1 class="text-4xl mt-4 font-extralight uppercase text-red-700">
+        Coming Soon...
+      </h1>
+    </div>
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: "Coming Soon",
+  },
+};
+</script>
